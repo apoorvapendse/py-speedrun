ec5005e1f1a43a8f31a6e9162cc01b60aac54363
ApoorvaPendse <102853901+apoorvapendse@users.noreply.github.com>
2023-06-23 09:40:27 +0530

    Create fstrings.py

---
+++ fstrings.py
@@ -0,0 +1,7 @@
+data = {
+    "name":"apoorva",
+    "age":19,
+    "college":"vit"
+}
+
+print("My name is {name}\nMy age is {age}\nMy college is {college}".format(name = data["name"], age = data["age"], college = data["college"]))
