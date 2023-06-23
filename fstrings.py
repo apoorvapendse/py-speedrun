ae8858db7e3808ddeed77a2e687bb07f174e9b70
ApoorvaPendse <102853901+apoorvapendse@users.noreply.github.com>
2023-06-23 09:42:30 +0530

    Update fstrings.py

--- fstrings.py
+++ fstrings.py
@@ -4,4 +4,8 @@ data = {
     "college":"vit"
 }
 
-print("My name is {name}\nMy age is {age}\nMy college is {college}".format(name = data["name"], age = data["age"], college = data["college"]))
+# print("My name is {name}\nMy age is {age}\nMy college is {college}".format(name = data["name"], age = data["age"], college = data["college"]))
+
+
+# we can also do
+print(f"My name is {data['name']}\nMy age is {data['age']}\nMy college is {data['college']}")
