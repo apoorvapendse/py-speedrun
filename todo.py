f8da61ede0d49d9bc97134f77a0062d79197b558
ApoorvaPendse <102853901+apoorvapendse@users.noreply.github.com>
2023-06-23 10:50:51 +0530

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,57 @@
+import os
+
+import time
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+todos = []
+
+
+
+def printTodos():
+    os.system("clear");
+    print(bcolors.OKGREEN+"Your Todos are:"+bcolors.ENDC)
+    for todo in todos:
+        print(bcolors.OKCYAN + bcolors.UNDERLINE+todo+bcolors.ENDC)
+    time.sleep(2)
+    os.system("clear")
+
+while True:
+    operation = int(input("1.Add Task\n2.Remove Task\n3.Display Tasks\n4.Exit\n"))
+    if operation==1:
+        os.system("clear")
+       
+        
+        task = input("enter the task to be added:")
+        todos.append(task);
+        
+        printTodos();
+    elif operation==2:
+        os.system("clear")
+        printTodos()
+        task = input("enter the task to be deleted:");
+        if task in todos:
+            todos.remove(task)
+            printTodos()
+        else:
+            os.system("clear")
+            print(bcolors.WARNING+"Entered task does not exist!"+bcolors.ENDC)
+            time.sleep(2);
+            os.system("clear")
+    elif operation==3:
+        os.system("clear")
+        printTodos()
+    else:
+        exit();
+        
